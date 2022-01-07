07b73a56bf0bc2a68301624c8450e3185e552e40
Mauro <mauro.gaudencio@ubi.pt>
2022-01-07 09:31:06 +0000

    Updated TabNet

--- Models/TabNet.py
+++ Models/TabNet.py
@@ -16,7 +16,8 @@ from sklearn.metrics import r2_score
 from statistics import mean
 import json
 
-fd = "\TabNet\Logs1\\" #files_directory
+images = False
+fd = "\TabNet\Logsx5\\" #files_directory
 m_mapes = []
 m_r2s = []
 m_maes = []
@@ -41,24 +42,6 @@ def time():
     current_time = now.strftime("%H:%M:%S")
     return current_time
 
-
-def normalize(df):
-    print("Normalization used is StandardScaler, using kfold=2")
-    # https://stackoverflow.com/questions/60998512/how-to-scale-all-columns-except-last-column
-    scalar = StandardScaler()
-    # uncomment for price normalization too and comment the one after it
-    #standardized_features = pd.DataFrame(scalar.fit_transform(df[all].copy()), columns=all)
-    standardized_features = pd.DataFrame(scalar.fit_transform(df[features].copy()), columns=features)
-    old_shape = df.shape
-    df.drop(features, axis=1, inplace=True)
-    df = pd.concat([df, standardized_features], axis=1)
-    assert old_shape == df.shape, "something went wrong!"
-
-    df.replace([np.inf, -np.inf], np.nan, inplace=True)
-    df.fillna(df.mean())
-
-    return df
-
 print("start: "+ str(time()))
 
 j = 0
@@ -70,11 +53,7 @@ for filename in datasets_list:
     print("Dataset: " + filename)
     df = pd.read_csv(datasets_dir + filename, index_col=None, header=0)
 
-    df = normalize(df)
-    df = df.reset_index()
-
     print("Dataset total size after normalization: " + str(df.shape))
-    print("Splitting 80/20")
 
     X = df[features]
     Y = df[labels]
@@ -82,10 +61,18 @@ for filename in datasets_list:
     X = X.to_numpy()
     Y = Y.to_numpy().reshape(-1, 1)
 
-
     X, X_test, y, y_test = train_test_split(X, Y, test_size=0.2)
+    print("Train/test division is 80/20")
 
-    for i in range(10):
+    print("Normalization used is StandardScaler")
+    scalar = StandardScaler()
+    scalerx = StandardScaler().fit(X)
+    scalery = StandardScaler().fit(y)
+    x_train = scalerx.transform(X)
+    y_train = scalery.transform(y)
+    x_test = scalerx.transform(X_test)
+
+    for i in range(5):
         j += 1
         print("j = " + str(j))
 
@@ -98,8 +85,8 @@ for filename in datasets_list:
                 X_train=X_train, y_train=y_train,
                 eval_set=[(X_train, y_train), (X_valid, y_valid)],
                 eval_metric=['rmse', 'mse', 'mae'],
-                max_epochs=15000,
-                patience = 150,
+                max_epochs=5000,
+                patience = 100,
                 num_workers=0,
                 drop_last=False
             )
@@ -108,32 +95,30 @@ for filename in datasets_list:
         print("Training ended: " + str(time()))
         print("Predictions starting:" + str(time()))
 
-
-        # plot losses
-        #try:
-        #    plt.plot(clf.history['loss'])
-        #    plt.savefig(cur_dir + fd + r'Loss_' + filename + str(j) + '.png')
-        #except:
-        #    pass
-
-        predictions = clf.predict(X_test)
+        normalized_predictions = clf.predict(X_test).reshape(-1, 1)
+        predictions = scalery.inverse_transform(normalized_predictions)
 
         #print("!!!!!!!!!!!!!!!!!!!!!!!!!!!True vs Preds!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
         #for idx,i in enumerate(y_test):
         #    print(str(y_test[idx]) + " --- " + str(predictions[idx]))
         #print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 
-        #plt.figure(figsize=(10, 10))
-        #plt.scatter(y_test, predictions, c='crimson')
-        #plt.yscale('log')
-        #plt.xscale('log')
-        #p1 = max(max(predictions), max(y_test))
-        #p2 = min(min(predictions), min(y_test))
-        #plt.plot([p1, p2], [p1, p2], 'b-')
-        #plt.xlabel('True Values', fontsize=15)
-        #plt.ylabel('Predictions', fontsize=15)
-        #plt.axis('equal')
-        #plt.savefig(cur_dir + fd + r'Predictions vs Actual_' + filename + str(j) + '.png')
+        if images == True:
+            plt.plot(clf.history['loss'])
+            plt.savefig(cur_dir + fd + r'Loss_' + filename + str(j) + '.png')
+            plt.close()
+            plt.figure(figsize=(10, 10))
+            plt.scatter(y_test, predictions, c='crimson')
+            plt.yscale('log')
+            plt.xscale('log')
+            p1 = max(max(predictions), max(y_test))
+            p2 = min(min(predictions), min(y_test))
+            plt.plot([p1, p2], [p1, p2], 'b-')
+            plt.xlabel('True Values', fontsize=15)
+            plt.ylabel('Predictions', fontsize=15)
+            plt.axis('equal')
+            plt.savefig(cur_dir + fd + r'Predictions vs Actual_' + filename + str(j) + '.png')
+            plt.close()
 
         print("Predictions ended: " + str(time()))
 
